a2abcdb83eabb510f8f4fd20933a2a5640e2393d
biswas08433 <BISWAS08433@GMAIL.COM>
2024-07-21 21:50:27 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="columns">
+    <div class="container pt-6 column is-one-third">
+      <!-- ADD-TODO-FORM -->
+      <TodoForm :add-todo="addTodo"></TodoForm>
+    </div>
+    <div class="column p-6">
+      <div class="fixed-grid has-3-cols">
+        <div class="grid">
+          <div v-for="(card, index) in todoCards" class="cell" :key="index">
+            <TodoCard
+              :todo-data="card"
+              :update-todo-card="updateTodoCard"
+              :delete-todo-card="deleteTodoCard"
+            />
+            <!-- {{ card }} -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoForm from "@/components/TodoForm.vue";
+import TodoCard from "@/components/TodoCard.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    TodoForm,
+    TodoCard,
+  },
+  created() {
+    this.getTodoCards();
+  },
+  data() {
+    return {
+      cardCounter: 0,
+      editModalActive: false,
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      todoCards: [],
+    };
+  },
+  methods: {
+    async getTodoCards() {
+      let res = await fetch("todoCards.json");
+      let data = await res.json();
+
+      this.cardCounter = data.length;
+      this.todoCards = data;
+    },
+    addTodo(formTodo) {
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      };
+      const formatter = new Intl.DateTimeFormat("en-US", options);
+      if (formTodo.todoTitle !== "") {
+        this.todoCards.push({
+          todoIndex: this.cardCounter,
+          todoTitle: formTodo.todoTitle,
+          todoDescription: formTodo.todoDescription,
+          todoPriority: formTodo.todoPriority,
+          todoCreationTime: formatter.format(Date.now()),
+        });
+      }
+      this.cardCounter++;
+    },
+    deleteTodoCard(index) {
+      let arrayIndex = this.todoCards.findIndex((e) => e.todoIndex === index);
+      this.todoCards.splice(arrayIndex, 1);
+    },
+    updateTodoCard(todo) {
+      let arrayIndex = this.todoCards.findIndex(
+        (e) => e.todoIndex === todo.todoIndex
+      );
+      this.todoCards[arrayIndex] = todo;
+    },
+  },
+};
+</script>
---
+++ src/components/TodoEditModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="modal" :class="{ 'is-active': active }">
+    <div class="modal-background" @click="deactivate"></div>
+    <div class="modal-content">
+      <form action="" method="post">
+        <div class="field">
+          <label class="label has-text-white">Title</label>
+          <div class="control">
+            <input
+              class="input"
+              v-model="updatedFormTodo.todoTitle"
+              type="text"
+              placeholder="e.g Market"
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label has-text-white">Description</label>
+          <div class="control">
+            <textarea
+              class="textarea"
+              rows="6"
+              placeholder="e.g. Rice, Dal, Atta"
+              v-model="updatedFormTodo.todoDescription"
+            />
+          </div>
+        </div>
+        <input
+          class="button is-danger is-pulled-right"
+          type="submit"
+          value="Update"
+          @click.prevent="dispatchTodoForUpdate"
+        />
+      </form>
+    </div>
+    <button
+      class="modal-close is-large"
+      aria-label="close"
+      @click="deactivate"
+    ></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["filler", "active", "deactivate", "updateTodoCard"],
+  data() {
+    return {
+      updatedFormTodo: {
+        todoTitle: this.filler.todoTitle,
+        todoDescription: this.filler.todoDescription,
+      },
+    };
+  },
+  methods: {
+    dispatchTodoForUpdate() {
+      this.updateTodoCard({
+        todoIndex: this.filler.todoIndex,
+        todoTitle: this.updatedFormTodo.todoTitle,
+        todoDescription: this.updatedFormTodo.todoDescription,
+        todoCreationTime: this.filler.todoCreationTime,
+      });
+      this.deactivate();
+    },
+  },
+};
+</script>
+
